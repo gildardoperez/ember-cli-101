dc9c2dca6cd2fb64daff48f12658934e4163a7de
Gildardo Perez <perezgildardo@Gils-MacBook-Pro.local>
2015-04-22 15:48:12 -0700

    added articles linked to friends

--- app/router.js
+++ app/router.js
@@ -12,13 +12,11 @@ export default Router.map(function() {
 Router.map(function() {
   this.resource('friends', function() {
     this.route('new');
-
-    this.route('show', {
-      path: ':friend_id'
+    this.route('show', { path: ':friend_id' }, function() {
+      this.resource('articles', function() { 
+      	this.route('new');
+      });
     });
-
-    this.route('edit', {
-      path: ':friend_id/edit'
-    });
+    this.route('edit', { path: ':friend_id/edit' });
   });
 });
