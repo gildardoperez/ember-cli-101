dcd955112304d5f2aa5d38f32d3c63383d9fa2ee
Gildardo Perez <perezgildardo@Gils-MacBook-Pro.local>
2015-04-23 15:13:44 -0700

    intergrated the Cumputed Property with Macros method for validation

--- app/controllers/friends/base.js
+++ app/controllers/friends/base.js
@@ -1,27 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  isValid: Ember.computed(
-    'email',
-    'firstName',
-    'lastName',
-    'twitter',
-    function() {
-      return !Ember.isEmpty(this.get('email')) &&
-        !Ember.isEmpty(this.get('firstName')) &&
-        !Ember.isEmpty(this.get('lastName')) &&
-        !Ember.isEmpty(this.get('twitter'));
-    }
+  hasEmail: Ember.computed.notEmpty('model.email'),
+  hasFirstName: Ember.computed.notEmpty('model.firstName'),
+  hasLastName: Ember.computed.notEmpty('model.lastName'),
+  hasTwitter: Ember.computed.notEmpty('model.twitter'),
+  isValid: Ember.computed.and(
+    'hasEmail',
+    'hasFirstName',
+    'hasLastName',
+    'hasTwitter'
   ),
   actions: {
     save: function() {
       if (this.get('isValid')) {
         var _this = this;
         this.get('model').save().then(function(friend) {
-          _this.transitionToRoute('articles', friend);
+          _this.transitionToRoute('friends');
         });
+        return true;
       } else {
-        this.set('errorMessage', 'You have to fill all the fields');
+        this.set('errorMessage', 'You have not fill all the fields');
         return false;
       }
     },
