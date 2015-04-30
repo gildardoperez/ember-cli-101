12a8dee78785f1bba70ec5f0f9de3c8c08b1c8be
Gildardo Perez <perezgildardo@Gils-MacBook-Pro.local>
2015-04-30 15:17:21 -0700

    added more pages transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,13 +1,19 @@
 export default function(){  
   this.transition(
-    this.fromRoute('tut1'),
-    this.toRoute('tut2'),
+    this.fromRoute('index'),
+    this.toRoute('friends.new'),
     this.use('toRight'),
     this.reverse('toLeft')
   );
     this.transition(
     this.fromRoute('index'),
     this.toRoute('friends'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
+    this.transition(
+    this.fromRoute('friends'),
+    this.toRoute('friends.new'),
     this.use('toRight'),
     this.reverse('toLeft')
   );
