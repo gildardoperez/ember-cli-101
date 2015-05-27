eb7481502cef860ed41510b7d0a8e26c829acadb
Gil Perez <gperez@whalerockindustries.com>
2015-05-27 13:31:19 -0700

    working with JavaScript plugins, installed momentJS date format

---
+++ tests/unit/utils/data-helpers-test.js
@@ -0,0 +1,10 @@
+import dataHelpers from '../../../utils/data-helpers';
+import { module, test } from 'qunit';
+
+module('dataHelpers');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = dataHelpers();
+  assert.ok(result);
+});
